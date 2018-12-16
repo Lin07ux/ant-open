45430b4a43ac6bcd9fb271802bfb21b0a1c151d5
Lin07ux <linshengli.linux7@icloud.com>
2018-12-16 23:56:15 +0800

    feat: 增加异常类

---
+++ src/Exception/BadRequestException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Description
+ *
+ * Author: Lin07ux
+ * Created_at: 2018-12-16 22:50:47
+ */
+
+namespace AntOpen\Exception;
+
+
+class BadRequestException extends \RuntimeException {}
---
+++ src/Exception/InvalidRequestContentException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 无效请求内容异常
+ *
+ * Author: Lin07ux
+ * Created_at: 2018-12-16 21:40:07
+ */
+
+namespace AntOpen\Exception;
+
+class InvalidRequestContentException extends \InvalidArgumentException {}
