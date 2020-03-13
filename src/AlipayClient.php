1c2845227cd1ec46de9a38e34944adad54355a5a
Lin07ux <linshengli.linux7@icloud.com>
2020-03-13 14:59:17 +0800

    style: 格式化代码样式，去除不需要的方法

--- src/AlipayClient.php
+++ src/AlipayClient.php
@@ -465,38 +465,29 @@ class AlipayClient
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
+        $encodeArray = [];
         $postBodyString = "";
-        $encodeArray = Array();
         $postMultipart = false;
 
         if (is_array($postFields) && 0 < count($postFields)) {
-
             foreach ($postFields as $k => $v) {
-                if ("@" != substr($v, 0, 1)) //判断是不是文件上传
-                {
-                    $postBodyString .= "$k=" . urlencode($this->characet($v, static::CHARSET)) . "&";
-                    $encodeArray[$k] = $this->characet($v, static::CHARSET);
-                } else //文件上传用multipart/form-data，否则用www-form-urlencoded
-                {
+                if ("@" !== substr($v, 0, 1)) {
+                    $postBodyString .= "$k=" . urlencode($v) . "&";
+                    $encodeArray[$k] = $v;
+                } else {
                     $postMultipart = true;
                     $encodeArray[$k] = new \CURLFile(substr($v, 1));
                 }
-
             }
 
-            unset ($k, $v);
             curl_setopt($ch, CURLOPT_POST, true);
 
             if ($postMultipart) {
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $encodeArray);
             } else {
                 curl_setopt($ch, CURLOPT_POSTFIELDS, substr($postBodyString, 0, -1));
+                curl_setopt($ch, CURLOPT_HTTPHEADER, ['content-type: application/x-www-form-urlencoded;charset='.static::CHARSET]);
             }
-        }
-
-        if (!$postMultipart) {
-            $headers = array('content-type: application/x-www-form-urlencoded;charset=' . static::CHARSET);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         }
 
         $response = curl_exec($ch);
@@ -516,26 +507,6 @@ class AlipayClient
     }
 
     /**
-     * 转换字符集编码
-     *
-     * @param  String  $data
-     * @param  String  $targetCharset
-     * @return string
-     */
-    function characet($data, $targetCharset)
-    {
-
-        if (!empty($data)) {
-            $fileType = static::CHARSET;
-            if (strcasecmp($fileType, $targetCharset) != 0) {
-                $data = mb_convert_encoding($data, $targetCharset, $fileType);
-            }
-        }
-
-        return $data;
-    }
-
-    /**
      * 校验值是否为空
      *
      * @param  mixed  $value
@@ -544,17 +515,5 @@ class AlipayClient
     private function isEmpty ($value)
     {
         return $value === false || $value === null || trim($value) === "";
-    }
-
-    /**
-     * 获取毫秒
-     *
-     * @return float
-     */
-    private function getMillisecond ()
-    {
-        list($s1, $s2) = explode(' ', microtime());
-
-        return (float)sprintf('%.0f', (floatval($s1) + floatval($s2)) * 1000);
     }
 }
